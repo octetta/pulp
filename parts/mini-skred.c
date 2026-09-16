dcceee5342e8b65e0a8ad884716a06273426c167
Joseph Stewart <joseph.stewart@gmail.com>
2026-09-16 13:01:34 +0200

    fix: remove accidental debug log wrapper in mini-skred

--- parts/mini-skred.c
+++ parts/mini-skred.c
@@ -95,7 +95,7 @@ static int mini_run_command(const char *line) {
   snprintf(buf, sizeof(buf), "%s", line);
   int r = skred_command(buf);
   char *log = skred_log();
-  if (strlen(log)) printf("LOG:\n%s---\n", log);
+  if (strlen(log)) printf("%s", log);
   if (r > 0) printf("r = %d\n", r);
   return r;
 }
